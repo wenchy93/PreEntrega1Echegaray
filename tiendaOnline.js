96a6822426e090f95cb7187b871947dc7dfc890f
Wenceslao Echegaray <echegaray.w@gmail.com>
2022-12-23 18:26:40 -0300

    Se modifica la funcion que solicita el numero de unidades a adquirir utilizando un bucle while para evitar que se envien datos NaN. Se elimina el bucle if ya que era innecesario. Se agregan variables para guardar los datos de los totales por juego.

--- tiendaOnline.js
+++ tiendaOnline.js
@@ -4,42 +4,39 @@
 
 let minecraft = "Minecraft";
 let minecraftPrecio = 500;
-let minecraftUnidades;
 
 let gta5 = "Grand Thef Auto: 5";
 let gta5Precio = 1200;
-let gta5Unidades;
 
 let lego = "LEGO Star Wars: The Skywalker Saga"
 let legoPrecio = 3000;
-let legoUnidades;
 
 let elden = "Elden Ring"
 let eldenPrecio = 4800;
-let eldenUnidades;
 
-let precioTotal;
-
-//Funcion que pregunta al usuario la cantidad de copias de un juego que desea adquirir.
-const pregunta1 = (juego, precio) => { return parseInt(prompt("Cuantas copias de " + juego + " quieres adquirir. Su precio es de $" + precio)); }
-
-//Bucle que ejecuta preguntas por cada uno de los cuatro juegos.
-for (let numeroDePreguntas = 0; numeroDePreguntas <= 5; numeroDePreguntas++) {
-    if (numeroDePreguntas == 0) {
-        minecraftUnidades = pregunta1(minecraft, minecraftPrecio );
+//Funcion que pregunta al usuario la cantidad de copias de un juego que desea adquirir y se asegura que no se ingrese un valor no numerico.
+const pregunta1 = (juego, precio) => {
+    let cantidad ="";
+    while (isNaN(cantidad) || cantidad === "") {
+        cantidad = prompt("Cuantas copias de " + juego + " quieres adquirir. Su precio es de " + precio);
     }
-    if (numeroDePreguntas == 1) {
-        gta5Unidades = pregunta1(gta5, gta5Precio);
-    }
-    if (numeroDePreguntas == 2) {
-        legoUnidades = pregunta1(lego, legoPrecio);
-    }
-    if (numeroDePreguntas == 3) {
-        eldenUnidades = pregunta1(elden, eldenPrecio);
-    }
-    if (numeroDePreguntas == 4) {
-        //Calculo del precio total y alerta que lo muestra en pantalla y ademas imprime las unidades por juego.
-        precioTotal = (((minecraftUnidades * minecraftPrecio) + (gta5Unidades * gta5Precio) + (legoUnidades * legoPrecio) + (eldenUnidades * eldenPrecio)) || 0);
-        alert("El precio total es de $" + precioTotal +". Llevas " + (minecraftUnidades || 0) + " Minecraft, " + (gta5Unidades || 0) + " Grand Thef Auto, " + (legoUnidades || 0) + " Lego Star Wars: The Skywalker Saga, " + (eldenUnidades || 0) + " Elden Ring.");
-    }
+    return parseInt(cantidad) || 0;
 }
+
+// Se realiza al usuario 4 preguntas por cada juego disponible.
+let minecraftUnidades = pregunta1(minecraft, minecraftPrecio);
+let gta5Unidades = pregunta1(gta5, gta5Precio);
+let legoUnidades = pregunta1(lego, legoPrecio);
+let eldenUnidades = pregunta1(elden, eldenPrecio);
+
+//Calculos de los totales.
+let minecraftTotal, gta5Total, legoTotal, eldenTotal, precioTotal; 
+
+minecraftTotal = minecraftPrecio * minecraftUnidades;
+gta5Total = gta5Precio * gta5Unidades;
+legoTotal = legoPrecio * legoUnidades;
+eldenTotal = eldenPrecio * eldenUnidades;
+precioTotal = minecraftTotal + gta5Total + legoTotal + eldenTotal;
+
+//Alerta Final que muestra el total de la compra y cuantas unidades 
+alert("El precio total es de $" + precioTotal + ". Llevas " + (minecraftUnidades || 0) + " " + minecraft + ", " + (gta5Unidades || 0) + " " + gta5 + ", " + (legoUnidades || 0) + " " + lego + ", " + (eldenUnidades || 0) + " " + elden + ".");
